4be4ce2a936bc26587be04a8b5c3baad49d057b4
Maicker M. Ravelo <63734332+dst4rk@users.noreply.github.com>
2022-03-11 03:51:02 -0400

    Add files via upload

--- RoutByPass.py
+++ RoutByPass.py
@@ -1,23 +1,59 @@
 
-import requests 
+from inspect import trace
+from operator import index
+import requests, time, signal, sys
+import contextlib
 
+attmp = 1
 avrouters = 0
 dsrouters = 0
-attmp = 1
-while attmp != 50:
-    targets = '192.168.3.'+ str(attmp)
+target = input("Please enter the Target or Targets: ")
+
+if int(target[10]) == 0:
+    index = list(target)
+    index[10] =""
+    target = "".join(index)
+    limit = int(input("introduzca la cantidad de ip a escanear (0 - 255): "))
+else:
+    attmp = int(target[10])
+    limit = attmp
+    
+print("\n Scaning...\n")
+time.sleep(2)
+validip = []
+
+while attmp <= limit:   
+    if  limit == attmp: 
+        ip = target
+    else:
+        ip = target + str(attmp)    
     attmp = attmp + 1
-    url = 'http://' + targets + '/'
+    url = 'http://' + ip + '/'
     try:
         r = requests.get(url, timeout=3)
         r.raise_for_status()
         if r.status_code == 200:
             avrouters += 1
-            print('Is running a Host on '+ url,'[+] status code : ',r.status_code)
+            validip.append(url)
+            print('[+] Login for Web Admin Panel Found on '+ url,'[+] status code : ',r.status_code)
     except Exception:
         dsrouters += 1
-        print('Is not running a Host on '+ url,'[-] status not found')
-print('Routers Avaliable: ',avrouters)  
-print('Routers not found: ', dsrouters)  
+        print('[+] Login for Web Admin Panel Not Found on '+ url,'[-] status not found')
+time.sleep(1)
+
+su_name = input("\nIntroduzca un nombre para el archivo de reporte: ")
+print("Printing scan results.. \n")
+time.sleep(3)
+
+file_path = su_name + '.txt'
+with open(file_path, "w") as o:
+    with contextlib.redirect_stdout(o):
+        print("Valid IP: ", validip)
+        print('Hosts Avaliable: ',avrouters)  
+        print('Hosts not found: ', dsrouters) 
+
+print('Hosts Avaliable: ',avrouters)  
+print('Hosts not found: ', dsrouters)  
 
         
+
